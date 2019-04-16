32bf8561d447ab446dc1d574d64d21fa67ed4e37
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-16 13:56:22 -0500

    Manually build product list component.ts, implement into app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { FormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
 
 import { AppComponent } from "./app.component";
+import { ProductListComponent } from "./products/product-list.component";
 
 // declarations is where our html looks to determine how to draw our component(s)
 // imports sets the modules we want to be available for use by our project's components
 // bootstrap is the start-up component used when beginning our app
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ProductListComponent],
   imports: [BrowserModule, FormsModule, HttpModule],
   providers: [],
   bootstrap: [AppComponent]
