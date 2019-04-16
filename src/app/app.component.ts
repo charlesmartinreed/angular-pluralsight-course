4a891a0fb1d8970dc0f84389dbb8e4e8595f2d88
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-16 13:19:27 -0500

    Demonstrate basics of creating a component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+// // We can import from angular because angular is modular - each component in angular is a module; @angular/core, @angular/http, etc.
+//
+// // import { memberName } from <angular library module name>
+//
+// import { Component } from "@angular/core";
+//
+// // We define metadata using the @Component decorator
+// // A decorator is a FUNCTION THAT ADDS METADATA TO A CLASS, ITS MEMBERS OR METHOD ARGUMENTS
+// // Scope of DECORATOR is limited to the feature that it decorates
+// // Always prefixed with @
+// // A Java language feature adapted into TypeScript
+//
+// // The selector defines the directive name for our component, which is a simple html tag embedded into our html page
+//
+// @Component({
+//   selector: "app-root",
+//   templateUrl: "./app.component.html",
+//   styleUrls: ["./app.component.css"]
+// })
+// export class AppComponent {
+//   title = "Hello world!";
+// }
 
+// We can import from angular because angular is modular - each component in angular is a module; @angular/core, @angular/http, etc.
+
+// import { memberName } from <angular library module name>
+import { Component } from "@angular/core";
+
+// We define metadata using the @Component decorator
+// A decorator is a FUNCTION THAT ADDS METADATA TO A CLASS, ITS MEMBERS OR METHOD ARGUMENTS
+// Scope of DECORATOR is limited to the feature that it decorates
+// Always prefixed with @
+// A Java language feature adapted into TypeScript
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
+
+// The selector defines the directive name for our component, which is a simple html tag embedded into our html page
 export class AppComponent {
-  title = 'app works!';
+  title: string = "Acme Product Management";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
+// declarations is where our html looks to determine how to draw our component(s)
+// imports sets the modules we want to be available for use by our project's components
+// bootstrap is the start-up component used when beginning our app
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, HttpModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
